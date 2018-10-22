743e949210066cf019cc030794fc2825ad9aa732
Jake Dillon <jake.dillon168@gmail.com>
2018-10-22 10:22:38 -0500

    app icon,

--- VideoGameLibraryIOS/ViewControllers/AddGameViewController.swift
+++ VideoGameLibraryIOS/ViewControllers/AddGameViewController.swift
@@ -10,15 +10,18 @@ import UIKit
 
 class AddGameViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     
-
+    
+    
+    let genres = ["Action", "RPG", "Puzzle", "Racing"]
+    
     @IBOutlet weak var gameTitle: UILabel!
- 
+    
     @IBOutlet weak var gameTitleTextField: UITextField!
     
     @IBOutlet weak var gameDescription: UILabel!
     
     @IBOutlet weak var gameDescriptionTextField: UITextView!
-   
+    
     @IBOutlet weak var ratingChoiceLabel: UILabel!
     
     @IBOutlet weak var ratingChoice: UISegmentedControl!
@@ -46,17 +49,48 @@ class AddGameViewController: UIViewController, UIPickerViewDelegate, UIPickerVie
      }
      */
     
-   // submits new game 
+    // submits new game
     @IBAction func submitButtonTapped(_ sender: Any) {
-    self.performSegue(withIdentifier: "unwindToGameList", sender: self)
+        guard let title = gameTitleTextField.text, title.trimmingCharacters(in: .whitespacesAndNewlines) != "", let gameDescription = gameDescriptionTextField.text, title.trimmingCharacters(in: .whitespacesAndNewlines) != "" else {
+            return
+        }
+        
+        var rating: String!
+        switch ratingChoice.selectedSegmentIndex {
+        case 0:
+            rating = "E"
+        case 1:
+            rating = "E-10"
+        case 2:
+            rating = "T"
+        case 3:
+            rating = "M"
+        case 4:
+            rating = "AO"
+        default:
+            rating = "E"
+            
+        }
+        let genre = genres[genreChoice.selectedRow(inComponent: 0)]
+        
+        let newGame = Game(title: title, description: gameDescription, genre: genre, rating: rating)
+        
+        GameManager.sharedInstance.addGame(game: newGame)
+        
+        self.performSegue(withIdentifier: "unwindToGameList", sender: self)
+        
     }
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        retrun 1
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return 
-    }
+        func numberOfComponents(in pickerView: UIPickerView) -> Int {
+            return 1
+        }
+        func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+            return genres[row]
+        }
+        func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+            return genres.count
+        }
     
 }
+
+
 
